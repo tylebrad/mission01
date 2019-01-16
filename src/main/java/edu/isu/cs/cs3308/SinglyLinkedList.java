d99e1501adb07749e1906ad14e7017236b2dfffe
tylebrad <tylebrad@Isu.edu>
2019-01-16 08:41:49 -0700

    Wed Morning
    gradlew build doesn't work. Failure point at the creation of a new SinglyLinkedList. Assuming its because my Singly class is empty OR i need to reference it somewhere

---
+++ src/main/java/edu/isu/cs/cs3308/SinglyLinkedList.java
@@ -0,0 +1,16 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import edu.isu.cs.cs3308.structures.List;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Class to Implement List functions
+ *
+ * @author Bradley Tyler
+ * CS-3308 Mission 01
+ */
+
+
